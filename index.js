8631ccf07c6ea2d26cd4fc72001fb6a162e07916
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-10 00:59:31 +0600

    тапшырма 12

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// const product = prompt('Введите логин:', '');
+
+// if (product === 'АДМИН') {
+
+// 	console.log = prompt('Введите пароль', '');
+
+// 	if (product === '2323tr') {
+// 		alert('Ваш пароль?');
+// 	} else if (product === '') {
+// 		alert('отмена');
+// 	} else if (product === '') {
+// 		alert('не верно');
+// 	} else {
+// 		alert('не верно');
+// 	} 
+// }    
+//     else if (product === '') {
+//     alert('отменено');
+// }  else {
+// 	alert('Else')
+// }
+
+
+
+
+
+
+let a = Number(prompt('жашын канчада'));
+
+if (a <= 30, a >= 18) {
+	console.log('ужас кино');
+}
+else if (a <= 17, a >= 12) {
+	console.log('фантастика');
+}
+else if (a <= 11, a > 1) {
+	console.log('мультик');
+}
+else {
+	console.log('романтика');
+}
